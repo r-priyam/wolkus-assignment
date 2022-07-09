a44d6291321dd8800f826ca510480ba7daf3889b
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-07-09 23:34:46 +0530

    feat: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <Head>
+                <title>Wolkus Assignment</title>
+            </Head>
+
+            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+                <h1>Something</h1>
+            </main>
+        </div>
+    );
+};
+
+export default Home;
