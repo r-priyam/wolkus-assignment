2139cc8ddc5ffa2fa87f3f6d7304c2fbe641ea5f
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-07-10 14:32:20 +0530

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        darkMode: 'class',
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+            },
+            colors: {
+                black: colors.zinc
+            }
+        }
     },
     plugins: []
 };
